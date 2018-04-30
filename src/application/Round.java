51fa25832cc715bb605367213111d668ace3da82
erdman3 <37053514+erdman3@users.noreply.github.com>
2018-04-29 20:11:52 -0500

    Fixed bugs with Round.java
    Some stuff is still hardcoded in here for my own testing

--- src/application/Round.java
+++ src/application/Round.java
@@ -14,6 +14,7 @@ public class Round {
 		this.roundNumber = roundNumber;
 		this.totalTeamNumber = roundCompetitors.size();
 		this.roundCompetitors = roundCompetitors;
+		this.roundMatchups = new ArrayList<Matchup>();
 		this.matchupWinners = new ArrayList<Competitor>();
 	}
 	
@@ -22,32 +23,32 @@ public class Round {
 		Collections.sort(this.roundCompetitors, new Comparator<Competitor>() {
 			@Override
 		    public int compare(Competitor c1, Competitor c2) {
-		        // -1 - less than, 1 - greater than, 0 - equal, all inversed for descending
-		        return c1.getSeed() > c2.getSeed() ? -1 : (c1.getSeed() < c2.getSeed()) ? 1 : 0;
+		        return c1.getSeed() < c2.getSeed() ? -1 : (c1.getSeed() > c2.getSeed()) ? 1 : 0;
 		    }
 		});
-			
 		for(int j = 0; j < this.totalTeamNumber / 2; j++) {
             this.roundMatchups.add(new Matchup(this.roundCompetitors.get(j), 
-            		this.roundCompetitors.get((this.totalTeamNumber / 2) - 1 - j)));
+            		this.roundCompetitors.get(this.roundCompetitors.size() - 1 - j)));
 		}
+		
 	}
 	
-	public ArrayList<Matchup> getMatchups() throws Exception {
+	public ArrayList<Matchup> getMatchups() {
 		if(this.roundMatchups != null) {
 			return this.roundMatchups;
 		}
 		
 		else {
-			throw new Exception("BAD");
+			return null;
 		}
 	}
 	
 	public ArrayList<Competitor> calculateMatchupWinners() {
 		for(int i = 0; i < this.roundMatchups.size(); i++) {
+			this.roundMatchups.get(i).setScore1(50);
+			this.roundMatchups.get(i).setScore2(75);
 			this.matchupWinners.add(this.roundMatchups.get(i).getWinner());
 		}
-		
 		return this.matchupWinners;
 	}
 	
