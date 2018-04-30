a1ae497f9a02cfe9f20ea156a7890abd3df3c9ce
erdman3 <37053514+erdman3@users.noreply.github.com>
2018-04-30 00:04:28 -0500

    Update Round.java -> adjusted seeding algorithm

--- src/application/Round.java
+++ src/application/Round.java
@@ -1,5 +1,6 @@
 package application;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -20,17 +21,21 @@ public class Round {
 	
 	
 	public void setMatchups() {
+		
+		if(this.roundNumber == 1) {
 		Collections.sort(this.roundCompetitors, new Comparator<Competitor>() {
 			@Override
 		    public int compare(Competitor c1, Competitor c2) {
 		        return c1.getSeed() < c2.getSeed() ? -1 : (c1.getSeed() > c2.getSeed()) ? 1 : 0;
 		    }
-		});
+		});  }
+		
 		for(int j = 0; j < this.totalTeamNumber / 2; j++) {
             this.roundMatchups.add(new Matchup(this.roundCompetitors.get(j), 
             		this.roundCompetitors.get(this.roundCompetitors.size() - 1 - j)));
 		}
-		
+		System.out.println("Competitors: " + this.roundCompetitors);
+		System.out.println("Matchups: " + this.roundMatchups);
 	}
 	
 	public ArrayList<Matchup> getMatchups() {
@@ -44,12 +49,14 @@ public class Round {
 	}
 	
 	public ArrayList<Competitor> calculateMatchupWinners() {
+		Random randGen = new Random();
 		for(int i = 0; i < this.roundMatchups.size(); i++) {
-			this.roundMatchups.get(i).setScore1(50);
-			this.roundMatchups.get(i).setScore2(75);
+			this.roundMatchups.get(i).setScore1(randGen.nextInt(1000));
+			this.roundMatchups.get(i).setScore2(randGen.nextInt(1000));
 			this.matchupWinners.add(this.roundMatchups.get(i).getWinner());
 		}
+		
+		System.out.println("Winners: " + this.matchupWinners);
 		return this.matchupWinners;
 	}
-	
 }
